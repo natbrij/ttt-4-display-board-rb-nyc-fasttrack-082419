3a8ea6aafe6659912aeb096be7117814042e0b1a
beneficial-trainer-3965 <natbrij@gmail.com>
2019-08-24 17:51:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
+  display_board(board)
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    display_board(board)
+
+end
